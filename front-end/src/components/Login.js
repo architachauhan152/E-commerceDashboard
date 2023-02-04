2c83015bffcd4631483461555d56ab6ca8b27fb6
Ritu Chauhan <93373957+architachauhan152@users.noreply.github.com>
2023-02-04 22:10:23 +0530

    login api integration done

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,9 +1,36 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 const Login=() =>{
     const [email, setEmail]=useState("");
     const [password, setPassword]=useState("")
-    const handleData=()=>{
+    const navigate=useNavigate()
+    useEffect(()=>{
+        const auth=localStorage.getItem('user');
+        if(auth){
+            navigate('/')
+        }
+
+    },[])
+    const handleData=async ()=>{
         console.log(email,password)
+        let result=await fetch("http://localhost:5000/login",{
+            method:"post",
+            body:JSON.stringify({email,password}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        result=await result.json()
+        console.log(result)
+        if(result.name){
+            localStorage.setItem("user",JSON.stringify(result))
+            navigate('/')
+
+        }else{
+            alert("please enter the correct details")
+        }
+
+        
 
     }
     return (
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,39 +1,45 @@
 import React from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Nav = () => {
-  const auth=localStorage.getItem('user')
-  const navigate=useNavigate();
-  const logout=()=>{
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const logout = () => {
     localStorage.clear();
-    navigate('/signup')
-  }
+    navigate("/signup");
+  };
 
   return (
     <div>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/">Products</Link>
-        </li>
-        <li>
-          <Link to="/add">Add Product</Link>
-        </li>
-        <li>
-          <Link to="/update">Update Product</Link>
-        </li>
-        
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>{auth?<Link onClick={logout} to="/signup">Logout</Link>:<Link to="/signup">Sign Up</Link>}
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-
-
-
-        </li>
-
-      </ul>
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update">Update Products</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link onClick={logout} to="/signup">
+              Logout
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
