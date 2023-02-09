c0eec3c8050eacba36e79106c79c1a593b0c13c0
Ritu Chauhan <93373957+architachauhan152@users.noreply.github.com>
2023-02-09 21:06:38 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ require("./db/config");
 const User = require("./db/User");
 const Product = require("./db/Product");
 
+const Jwt = require("jsonwebtoken");
+const jwtkey = "e-com";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,8 +16,12 @@ app.post("/register", async (req, res) => {
   let result = await user.save();
   result = result.toObject();
   delete result.password;
-
-  res.send(result);
+  Jwt.sign({ result }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+    if (err) {
+      res.send({ result: "user not found something went wrong" });
+    }
+    res.send({ result, auth: token });
+  });
 });
 
 app.post("/login", async (req, res) => {
@@ -22,7 +29,12 @@ app.post("/login", async (req, res) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      res.send(user);
+      Jwt.sign({ user }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+          res.send({ result: "user not found something went wrong" });
+        }
+        res.send({ user, auth: token });
+      });
     } else {
       res.send({ result: "No User Found" });
     }
@@ -66,15 +78,14 @@ app.put("/product/:id", async (req, res) => {
   );
   res.send(result);
 });
-app.get('/search/:key', async (req,res) =>{
-  let result=await Product.find({
-    "$or":[
-      {name:{$regex:req.params.key}},
-      {company:{$regex:req.params.key}},
-      {category:{$regex:req.params.key}}
-    ]
-
-  })
-  res.send(result)
-})
+app.get("/search/:key", async (req, res) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  res.send(result);
+});
 app.listen(5000);
--- front-end/src/components/ProductList.js
+++ front-end/src/components/ProductList.js
@@ -1,74 +1,80 @@
 import React, { useState, useEffect } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 const ProductList = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        getProducts();
-    }, []);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    const getProducts = async () => {
-        let result = await fetch("http://localhost:5000/products");
-        result = await result.json();
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:5000/products"
+        // headers:{
+        //     authorization:JSON.parse(localStorage.getItem('token'))
+        // }
+    );
+    result = await result.json();
+    setProducts(result);
+  };
+  const deleteproduct = async (id) => {
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "Delete",
+    });
+    result = await result.json();
+    if (result) {
+      getProducts();
+    }
+  };
+  const SearchHandle = async (e) => {
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
         setProducts(result);
-    };
-    const deleteproduct= async (id)=>{
-        let result=await fetch(`http://localhost:5000/product/${id}`,{
-        method:"Delete"
-    })
-    result=await result.json();
-    if(result){
-        getProducts()
+      }
+    } else {
+      getProducts();
     }
-    }
-    const SearchHandle= async (e)=>{
-        let key=e.target.value;
-        if(key){
-        let result= await fetch(`http://localhost:5000/search/${key}`)
-        result=await result.json()
-        if(result){
-        setProducts(result)
-        }
-    }else{
-        getProducts();
-    }
+  };
 
-    }
-
-    return (
-        <div className="productlist">
-            <h1>Product List</h1>
-            <input className="search-input" type="text" placeholder="Search Product...." onChange={SearchHandle}/>
-            <ul>
-                <li>Serial N.</li>
-                <li>Name</li>
-                <li>Price</li>
-                <li>Category</li>
-                {/* <li>Company</li> */}
-                <li>Opration</li>
-            </ul>
-            {
-                products.length>0 ?products.map((item, index) =>
-                    <ul key={item._id}>
-                        <li>{index + 1}</li>
-                        <li>{item.name}</li>
-                        <li>{item.price}</li>
-                        <li>{item.category}</li>
-                        {/* <li>{item.company}</li> */}
-                        <li><button style={{}} onClick={()=>deleteproduct(item._id)}>Delete</button>
-                        <Link to={"/update/"+item._id}>Update</Link>
-                        
-                        </li>
-
-                        </ul>
-
-                ):
-                <h1>No Product Found..</h1>
-            }
-            
-
-
-        </div>
-    )
-}
+  return (
+    <div className="productlist">
+      <h1>Product List</h1>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search Product...."
+        onChange={SearchHandle}
+      />
+      <ul>
+        <li>Serial N.</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>Category</li>
+        {/* <li>Company</li> */}
+        <li>Opration</li>
+      </ul>
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            {/* <li>{item.company}</li> */}
+            <li>
+              <button style={{}} onClick={() => deleteproduct(item._id)}>
+                Delete
+              </button>
+              <Link to={"/update/" + item._id}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No Product Found</h1>
+      )}
+    </div>
+  );
+};
 
 export default ProductList;
